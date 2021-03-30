cb9e926b30f14f7412db8111dd20330a139cb633
Zoro <76905656+Zoro-YT@users.noreply.github.com>
2021-03-30 20:26:48 +0800

    Add files via upload

---
+++ Episode 3/commands/BOT COMMANDS/eval.js
@@ -0,0 +1,9 @@
+module.exports = ({
+    name: "eval",
+    code: 
+`
+$eval[$message]
+$argsCheck[>1;Wrong Usage Use \`!z eval $samplecmd\`]
+$onlyForIDs[720513988114251827;:x: You Are Not My Developer!]
+`
+});
--- Episode 3/index.js
+++ Episode 3/index.js
@@ -1,1 +1,41 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+ 
+app.get('/', (req, res) => res.send('ZORO YT'));
+ 
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
+const dbd = require("dbd.js")
+ 
+const bot = new dbd.Bot({
+token: "ODI1MjM5MTg4NTg0NjYwOTkz.YF7B4g.jcNew1rWt54rhsHaE2SbMG01HwM", 
+prefix: "!z" 
+})
+ 
+bot.onMessage()
+ 
+const fs = require('fs')
+
+const folders = fs.readdirSync("./commands/")
+
+for (const files of folders) {
+const folder = fs.readdirSync(`./commands/${files}/`).filter(file => file.endsWith(".js"))
+
+for (const commands of folder) {
+const command = require(`./commands/${files}/${commands}`) 
+bot.command(command)
+} 
+}
+
+bot.status({
+  text: "Test Bot",
+  type: "PLAYING",
+  time: 12
+})
+
+bot.status({
+  text: "Subscribe To Zoro",
+  type: "WATCHING",
+  time: 12
+})
